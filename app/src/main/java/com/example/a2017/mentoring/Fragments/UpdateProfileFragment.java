4fc38d6050248c2e65496066a55a5f4f095f9234
tsofenbaqa <tsofenbaqa@gmail.com>
2017-02-15 20:45:29 +0200

    update afif 15.2 20:45

--- app/src/main/java/com/example/a2017/mentoring/Fragments/UpdateProfileFragment.java
+++ app/src/main/java/com/example/a2017/mentoring/Fragments/UpdateProfileFragment.java
@@ -1,11 +1,22 @@
 package com.example.a2017.mentoring.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.example.a2017.mentoring.R;
 
 /**
@@ -14,11 +25,86 @@ import com.example.a2017.mentoring.R;
 
 public class UpdateProfileFragment extends Fragment
 {
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "MainActivity";
+
+    CoordinatorLayout coordinatorLayout;
+    FloatingActionButton btnSelectImage;
+    ImageView imgView;
+    Button upcv, upmark;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_update_profile,container,false);
+
+        coordinatorLayout = (CoordinatorLayout)view.findViewById(R.id.cor);
+        btnSelectImage = (FloatingActionButton) view.findViewById(R.id.fab_btn);
+        upcv=(Button)view.findViewById(R.id.upcv_btn);
+        upmark=(Button)view.findViewById(R.id.upmark_btn);
+
+        imgView = (ImageView) view.findViewById(R.id.main_backdrop);
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+            }
+        });
+
+        upcv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+
+            }
+        });
+
+        upmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+            }
+        });
         return view;
     }
+
+    void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Get the path from the Uri
+                    String path = getPathFromURI(selectedImageUri);
+                    Log.i(TAG, "Image Path : " + path);
+                    // Set the image in ImageView
+                    imgView.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getActivity().getApplicationContext().getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
+
 }
+
