d80214a1f3b889e23fa63513a59813676f66625e
tsofenbaqa@gmail.com <tsofenbaqa@gmail.com>
2017-02-17 10:11:26 +0200

    update abu elula work done by afif

---
+++ app/src/main/java/com/example/a2017/mentoring/Fragments/RegisterFragment.java
@@ -0,0 +1,21 @@
+package com.example.a2017.mentoring.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 2017 on 17/02/2017.
+ */
+
+public class RegisterFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
