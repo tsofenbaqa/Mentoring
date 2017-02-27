b9dac95827cb39311c80ef2e603d6ebc5b3ada7f
tsofenbaqa <tsofenbaqa@gmail.com>
2017-02-27 19:40:53 +0200

    Muhamamd Abu Elula 27/2/2017 Registration Validation & progress bar

--- app/src/main/java/com/example/a2017/mentoring/Fragments/RegisterFragment.java
+++ app/src/main/java/com/example/a2017/mentoring/Fragments/RegisterFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -23,85 +24,85 @@ import com.example.a2017.mentoring.RetrofitApi.ApiClientRetrofit;
 import com.example.a2017.mentoring.RetrofitApi.ApiInterfaceRetrofit;
 import com.example.a2017.mentoring.Utils.Preferences;
 
+import com.gospelware.liquidbutton.LiquidButton;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RegisterFragment extends Fragment {
-  EditText fnameet, lnameet, emailet, pass1et, pass2et,phone;
-  String sfnameet, slnameet, semailet, spass1et, spass2et,sphone,stype;
+  EditText first_name, last_name, email, first_password, second_password, phone_number;
+  String _first_name, _last_name, _email, _first_password, _second_password, _phone_number, _type;
   Spinner type;
   Button ok;
+  ProgressBar progressBar;
+  LiquidButton liquidButton;
 
-  @Nullable
-  @Override
+  @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     {
       super.onCreate(savedInstanceState);
       View view = inflater.inflate(R.layout.register_fragment, container, false);
-      fnameet = (EditText) view.findViewById(R.id.fname);
-      lnameet = (EditText) view.findViewById(R.id.lname);
-      emailet = (EditText) view.findViewById(R.id.email);
-      pass1et = (EditText) view.findViewById(R.id.pass1);
-      pass2et = (EditText) view.findViewById(R.id.pass2);
-      phone=(EditText)view.findViewById(R.id.phone2) ;
-      type = (Spinner) view.findViewById(R.id.type);
+      first_name = (EditText) view.findViewById(R.id.reg_first_name);
+      last_name = (EditText) view.findViewById(R.id.reg_last_name);
+      email = (EditText) view.findViewById(R.id.reg_email_address);
+      first_password = (EditText) view.findViewById(R.id.reg_first_password);
+      second_password = (EditText) view.findViewById(R.id.reg_second_password);
+      phone_number = (EditText) view.findViewById(R.id.reg_phone_number);
+      type = (Spinner) view.findViewById(R.id.reg_type);
       ok = (Button) view.findViewById(R.id.btnok);
+      liquidButton=(LiquidButton)view.findViewById(R.id.reg_done_animation);
+      progressBar = (ProgressBar)view.findViewById(R.id.reg_progressBar);
       getType();
-      registerButoon();
+      doAnimation();
+      registerButton();
       return view;
     }
   }
-  private void registerButoon()
-  {
+
+  private void registerButton() {
     ok.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v)
-      {
+      @Override public void onClick(View v) {
         readTextFromEditText();
-
-//        if ((isValidEmail(emailet.getText()) &&
-//                  (isValidname(fnameet.getText().toString(), "First")) &&
-//                  (isValidname(lnameet.getText().toString(), "Last") &&
-//                          (isValidPassword(pass1et.getText().toString(), pass2et.getText().toString())))))
-//          {}
-
-        Register register = new Register(semailet,spass1et,sfnameet,slnameet,stype,sphone);
-        sendRegisterToServer(register);
-
-
-      }
-    });
-  }
-  private void getType()
-  {
-    type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-    {
-      @Override
-      public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-      {
-        stype=type.getItemAtPosition(position).toString();
-      }
-
-      @Override
-      public void onNothingSelected(AdapterView<?> parent)
-      {
+        if(isAllValid()) {
+          Register register = new Register(_email, _first_password, _first_name, _last_name, _type, _phone_number);
+          sendRegisterToServer(register);
+        }
 
       }
     });
   }
 
-  public final static boolean isValidEmail(CharSequence target) {
-    if (TextUtils.isEmpty(target)) {
+  private void getType() {
+    type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        _type = type.getItemAtPosition(position).toString();
+      }
+
+      @Override public void onNothingSelected(AdapterView<?> parent) {
+
+      }
+    });
+  }
+
+  public boolean isValidPhoneNumber() {
+    if(android.util.Patterns.PHONE.matcher(phone_number.getText().toString()).matches()) {
+      if((/*phone_number.getText().toString().length()<10)||*/(phone_number.getText().toString().length()>10)))
+      phone_number.setError("Invalid Phone number");
       return false;
     } else {
-      return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+      return true;
     }
   }
 
-  public final boolean isValidname(String s, String n) {
-    if (s.isEmpty()) {
-      fnameet.setError("wrong name");
+  public boolean isValidEmail() {
+    CharSequence target = email.getText().toString();
+//    if (TextUtils.isEmpty(target)) {
+    if(!android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches())
+    {
+      email.setError("Invalid Email Address");
       return false;
     }
     {
@@ -109,63 +110,126 @@ public class RegisterFragment extends Fragment {
     }
   }
 
-  public final boolean isValidPassword(String first, String second) {
-    if ((!first.isEmpty() && (!second.isEmpty()))) {
-      if (first.equals(second)) {
-        return true;
-      } else {
-        pass1et.setError("wrong password");
-        return false;
-      }
+  public boolean isValidFirstName() {
+    String regx = "[a-zA-Z]+\\.?";
+    Pattern pattern = Pattern.compile(regx, Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(first_name.getText().toString());
+    if (!matcher.matches()) {
+      first_name.setError("Invalid First Name");
+      return false;
     }
-    return false;
+    else return true;
   }
 
-  public void sendRegisterToServer(Register register)
+  public boolean isValidLastName() {
+    String regx = "[a-zA-Z]+\\.?";
+    Pattern pattern = Pattern.compile(regx, Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(last_name.getText().toString());
+    if (!matcher.matches()) {
+      last_name.setError("Invalid Last Name");
+      return false;
+    }
+    else return true;
+  }
+
+  public final boolean isValidPassword() {
+    Pattern pattern;
+    Matcher matcher;
+    String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[@_.]).*$";
+    pattern = Pattern.compile(PASSWORD_PATTERN);
+    matcher = pattern.matcher(first_password.getText().toString());
+    if(!matcher.matches())
+    {
+      first_password.setError("Invalid Password,Password must contain 0-9,A-Z and special characters");
+      return false;
+
+    }
+    else return true;
+  }
+
+  public final boolean isValidPasswords()
   {
+
+    if ((!first_password.getText().toString().isEmpty()) && (!second_password.getText()
+        .toString()
+        .isEmpty()))
+    {
+      if (isValidPassword())
+        if (first_password.getText().toString().equals(second_password.getText().toString())) return true;
+        else {
+          second_password.setError("Passwords doesn't match");
+          return false;
+        }
+    }
+    else return true;
+    return false;
+
+  }
+
+  public void sendRegisterToServer(Register register) {
+    progressBar.setVisibility(View.VISIBLE);
     ApiInterfaceRetrofit retrofit = ApiClientRetrofit.getClient().create(ApiInterfaceRetrofit.class);
     Call<String> addNewUser = retrofit.addNewUser(register);
-    addNewUser.enqueue(new Callback<String>()
-    {
-      @Override
-      public void onResponse(Call<String> call, Response<String> response)
-      {
-        Log.d( "onResponse: ","done");
-        if(response.code() == 200 ||response.code() == 204)
-        {
-          Toast.makeText(getContext(),"is done " , Toast.LENGTH_LONG).show();
-          Intent i2 = new Intent(getContext(), MainActivity.class);
-          getContext().startActivity(i2);
-          Preferences.setLogin(true,getContext());
+    addNewUser.enqueue(new Callback<String>() {
+      @Override public void onResponse(Call<String> call, Response<String> response) {
+        Log.d("onResponse: ", "done");
+        if (response.code() == 200 || response.code() == 204) {
+          progressBar.setVisibility(View.GONE);
+          liquidButton.startPour();
 
-        }
-        else if(response.code() == 302 )
-        {
-          Toast.makeText(getActivity(),"we are sorry the user exists try another user " , Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-          Toast.makeText(getActivity(),"error " , Toast.LENGTH_LONG).show();
-
+        } else if (response.code() == 302) {
+          Toast.makeText(getActivity(), getResources().getString(R.string.userExists), Toast.LENGTH_LONG).show();
+        } else {
+          Toast.makeText(getActivity(), getResources().getString(R.string.error_general), Toast.LENGTH_LONG).show();
         }
       }
 
-      @Override
-      public void onFailure(Call<String> call, Throwable t)
-      {
-        Toast.makeText(getActivity(),"onFailure" , Toast.LENGTH_LONG).show();
+      @Override public void onFailure(Call<String> call, Throwable t) {
+        Toast.makeText(getActivity(), getResources().getString(R.string.failure), Toast.LENGTH_LONG).show();
+        progressBar.setVisibility(View.GONE);
       }
     });
   }
 
-  private void readTextFromEditText()
-  {
-    sfnameet = fnameet.getText().toString();
-    slnameet=lnameet.getText().toString();
-    semailet=emailet.getText().toString();
-    spass1et=pass1et.getText().toString();
-    spass2et=pass2et.getText().toString();
-    sphone=phone.getText().toString();
+  private void readTextFromEditText() {
+    _first_name = first_name.getText().toString();
+    _last_name = last_name.getText().toString();
+    _email = email.getText().toString();
+    _first_password = first_password.getText().toString();
+    _second_password = second_password.getText().toString();
+    _phone_number = phone_number.getText().toString();
+  }
+
+  private void doAnimation() {
+    liquidButton.setPourFinishListener(new LiquidButton.PourFinishListener() {
+      @Override public void onPourFinish() {
+        Toast.makeText(getContext(), "is done ", Toast.LENGTH_LONG).show();
+        Intent i2 = new Intent(getContext(), MainActivity.class);
+        getContext().startActivity(i2);
+        Preferences.setLogin(true, getContext());
+      }
+
+      @Override public void onProgressUpdate(float progress) {
+
+      }
+    });
+  }
+
+  public boolean isAllValid() {
+    if (isValidFirstName()) {
+      if (isValidLastName()) {
+        if (isValidEmail()) {
+          if (isValidPhoneNumber()) {
+            if (isValidPasswords()) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    else
+      return false;
+    return false;
   }
 }
 
